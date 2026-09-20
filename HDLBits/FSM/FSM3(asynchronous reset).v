f8be3b26d0f76b7a13ccbcd37fab9e5435987016
huihui-emo <zjh.4846159@gmail.com>
2026-09-20 11:02:11 +0800

    Add files via upload

---
+++ HDLBits/FSM/FSM3(asynchronous reset).v
@@ -0,0 +1,34 @@
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out); //
+	
+    parameter A=2'd0,B=2'd1,C=2'd2,D=2'd3;
+    reg [1:0]state,next_state;
+    always@(posedge clk or posedge areset)
+        begin
+            if(areset) state<=2'd0;
+            else state<=next_state;
+        end
+    always@(*)begin
+        case(state)
+            A: if(in) next_state=B;
+            else next_state=A;
+            B: if(in) next_state=B;
+            else next_state=C;
+            C: if(in) next_state=D;
+            else next_state=A;
+            D: if(in) next_state=B;
+            else next_state=C;
+        endcase
+    end
+    assign out=(state==D);
+        
+    // State transition logic
+
+    // State flip-flops with asynchronous reset
+
+    // Output logic
+
+endmodule
