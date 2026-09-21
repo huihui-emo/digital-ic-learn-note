e217e3e8fcab0905bff94897cc65eeb2de588918
huihui-emo <zjh.4846159@gmail.com>
2026-09-21 10:52:59 +0800

    Create 01_Fsm_as.v

---
+++ HDLBits/FSM/01_Fsm_as.v
@@ -0,0 +1,29 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to state B
+    input in,
+    output out);//  
+
+    parameter A=0, B=1; 
+    reg state, next_state;
+ 
+    always @(*) begin    // This is a combinational always block
+        case(state)
+            A: if(in) next_state=A;
+               else next_state=B;
+            B: if(in) next_state=B;
+               else next_state=A;
+        endcase
+    // State transition logic
+    end
+
+    always @(posedge clk, posedge areset) begin    // This is a sequential always block
+        if(areset) state<=B;
+        else state<=next_state;// State flip-flops with asynchronous reset
+    end
+
+    assign out=(state==B)?1'b1:1'b0;
+    // Output logic
+    // assign out = (state == ...);
+
+endmodule
